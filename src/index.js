040507ab8abd25a6e31aae4368ef6458f1e2f93d
Sare <evilape@web.de>
2021-11-09 20:03:36 +0100

    unfinished stream

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 const express = require('express')
-const {Storage} = require('@google-cloud/storage')
+// Imports the Google Cloud client library.
+const { Storage } = require('@google-cloud/storage');
 
-const storage = new Storage();
+const app = express()
+const PORT = 8082 //process.env.PORT
+const bucketName = "vstorage39f"
 
-// Creates a client from a Google service account key
-const storage = new Storage({keyFilename: 'key.json'});
-const bucketName = 'vstorage39f';
-
-async function createBucket() {
-  // Creates the new bucket
-  await storage.createBucket(bucketName);
-  console.log(`Bucket ${bucketName} created.`);
+// Makes an authenticated API request.
+async function createBlobService() {
+  const storage = await new Storage();
+  return storage
 }
 
-createBucket().catch(console.error);
+
+async function getFileName() {
+  const storage = await createBlobService()
+  const [files] = await storage.bucket(bucketName).getFiles();
+
+  console.log(files[0].name)
+  const filename = await storage.bucket(bucketName).file('sample1.mp4')
+  await console.log(filename)
+  return files[0].name
+}
+
+
+app.get("/video", async (req, res) => {
+  const storage = await createBlobService()
+
+  res.writeHead(200, {
+    "content-length": 1,
+    "content-type": "video/mp4"
+  })
+
+  await storage
+    .bucket(bucketName)
+    .file('sample1.mp4')
+    .createReadStream(err => {
+      if (err) {
+        res.sendStatus(500)
+      }
+    })
+    .pipe(res, err => {
+      if (err) {
+        res.sendStatus(500)
+      }
+
+      res.writeHead(200, {
+        "Content-Length": properties.contentLength,
+        "content-type": "video/mp4"
+      })
+    })
+    .on('finish', () => {
+    });
+})
+
+app.listen(PORT, () => {
+  console.log('running')
+})
